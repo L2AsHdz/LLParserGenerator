f01ddc40636a8244d67499f42640bd1ca626e15e
L2AsHdz <leonidas0197@gmail.com>
2021-04-16 01:31:25 -0600

    Se agrego indice para obtener siguiente termindo del lado derecho

--- src/models/LL1/PrimerosCalculator.ts
+++ src/models/LL1/PrimerosCalculator.ts
@@ -16,19 +16,15 @@ export class PrimerosCalculator {
         //for (let i in this.producciones) {
         let nameLeft: string = this.producciones[0].getLeftSide();
         let pTemp: Array<Produccion> = new Array<Produccion>();
-        this.producciones.forEach(p => {
-            if (p.getLeftSide() == nameLeft) {
-                pTemp.push(p);
-            }
-        });
-        this.calcularPrimeros(nameLeft, pTemp);
+        this.addProduccionesTemp(pTemp, nameLeft);
+        this.calcularPrimeros(nameLeft, pTemp, 0);
     }
 
-    private calcularPrimeros(nameLeft: string, pTemp: Array<Produccion>) {
+    private calcularPrimeros(nameLeft: string, pTemp: Array<Produccion>, indexRight: number) {
         let nTermTblLeft: NoTerminal = this.getNoTermTbl(nameLeft);
 
         for (let p of pTemp) {
-            let right: Termino = p.getRightSide()[0];
+            let right: Termino = p.getRightSide()[indexRight];
             if  (right.getIsTerminal()) {
                 this.addToPrimeros(nTermTblLeft.getPrimeros(), right.getNombre());
             } else {
@@ -39,19 +35,19 @@ export class PrimerosCalculator {
                             if (nTermTblRight.getPrimeros().length == 0) {
                                 let left = right.getNombre();
                                 let prodTemp: Array<Produccion> = [];
-                                this.producciones.forEach(p => {
-                                    if (p.getLeftSide() == left) {
-                                        prodTemp.push(p);
-                                    }
-                                });
-                                this.calcularPrimeros(left, prodTemp);
+                                this.addProduccionesTemp(prodTemp, left);
+                                this.calcularPrimeros(left, prodTemp, 0);
                             }
                             nTermTblRight.getPrimeros().forEach(p => {
                                 this.addToPrimeros(nTermTblLeft.getPrimeros(), p);
                             });
                         }
                     } else {
-                        //Evaluar siguiente
+                        if (nameLeft != right.getNombre()) {
+                            let nTermTblRight: NoTerminal = this.getNoTermTbl(right.getNombre());
+                        } else {
+                            //Evaluar solo siguientes siguientes
+                        }
                     }
                 }
             }
@@ -66,5 +62,13 @@ export class PrimerosCalculator {
         if (array.find(e => e == element) == undefined) {
             array.push(element);
         }
+    }
+
+    private addProduccionesTemp(temp: Array<Produccion>, nameLeft: string) {
+        this.producciones.forEach(p => {
+            if (p.getLeftSide() == nameLeft) {
+                temp.push(p);
+            }
+        });
     }
 }
